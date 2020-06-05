313829d70e3f6be87d773476f4ef8a3640ff7ed5
Danyihza <55189649+Danyihza@users.noreply.github.com>
2020-06-05 11:19:21 +0700

    Revert "foot"
    This reverts commit 3a492a2fad4efc50f2f88dd4b4c19503f9c697e1.

--- application/views/main/templates/bottom.php
+++ application/views/main/templates/bottom.php
@@ -1,4 +1,24 @@
+ <!-- Copyright -->
 
+ <div class="copyright">
+     <div class="container">
+         <div class="row">
+             <div class="col">
+
+                 <div class="copyright_container d-flex flex-sm-row flex-column align-items-center justify-content-start">
+                     <div class="copyright_content">
+                         <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                         Copyright &copy;<script>
+                             document.write(new Date().getFullYear());
+                         </script> All rights reserved - Media Ar-Raihan
+                         <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
+ </div>
 
  <script src="<?= base_url('assets/ckeditor/'); ?>ckeditor.js"></script>
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
